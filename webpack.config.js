a3a250f300fffd650877a1d365970184150a314b
Schelde Jonas <scheldejonas@gmail.com>
2017-03-21 14:21:34 +0100

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,13 @@ var webpackConfig = {
     filename: "index.js"
   },
   module: {
-    loaders: []
+    loaders: [
+        {
+            loader: "babel-loader",
+            test: /.js$/,
+            exclude: /node_modules/
+        }
+    ]
   }
 };
 
